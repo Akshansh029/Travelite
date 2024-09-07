7c843072a08665c00d024999805498b69edf7593
Aytaditya <adityaaryan531@gmail.com>
2024-09-08 00:39:22 +0530

    view trip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const {
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
   ],
   prefix: "",
   theme: {
